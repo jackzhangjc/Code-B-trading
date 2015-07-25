e2714ccd4a89262922f4331a3ea1b3292f2dde7b
Ri Xu <rixu.jack@gmail.com>
2015-07-25 10:34:52 -0400

    thrid shit

--- codeb/client.m.cpp
+++ codeb/client.m.cpp
@@ -11,11 +11,48 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
 using namespace galik;
 using namespace galik::net;
+
+socketstream ss;
+
+struct Stock {
+    string ticker;
+    double netWorth;
+    double dividendRatio;
+    double volatility;
+    
+    Stock(string ticker, double netWorth, double dividendRatio, double volatility): ticker(ticker), netWorth(netWorth), dividendRatio(dividendRatio), volatility(volatility){}
+};
+
+struct MyStock {
+    string ticker;
+    int shares;
+    double dividendRatio;
+    
+    MyStock(string ticker, int shares, double dividendRatio): ticker(ticker), shares(shares), dividendRatio(dividendRatio){}
+};
+
+struct MyOrder {
+    string type;
+    string ticker;
+    double price;
+    int shares;
+    
+    MyOrder(string type, string ticker, double price, int shares): type(type), ticker(ticker), price(price), shares(shares){}
+};
+
+//struct Order {
+//    string type;
+//    double price;
+//    int shares;
+//    
+//    Order(string type, double price, int shares): type(type), price(price), shares(shares){}
+//};
 
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
 	size_t start = 0, end = 0;
@@ -27,83 +64,281 @@ std::vector<std::string> &split(const std::string &s, char delim, std::vector<st
 	return elems;
 }
 
-vector<stock> stocks;
-socketstream ss;
-string name = "TripleG";
-string password = "sjj";
-
-struct stock {
-	string ticker;
-	double earnings;
-	int shareNum;
-	double dividenRatio;
-};
-
-void InitialStocks() {
-
+// COMMANDS
+double myCash() {
+    double n;
+    ss << "MY_CASH " << endl;
+    string line;
+    if (ss.good() && !ss.eof()) {
+        getline(ss, line);
+        stringstream s;
+        s << line;
+        string t;
+        s >> t;
+        s.clear();
+        s.ignore();
+        s << line;
+        s >> n;
+    }
+    return n;
 }
 
-void fetchStocks() {
-
+void showMyCash() {
+    cout << myCash() << endl;
 }
 
-stock getHighestDividen() {
-
+vector<MyStock> mySecurities() {
+    ss << "MY_SECURITIES " << endl;
+    string line;
+    vector<MyStock> v;
+    if (ss.good() && !ss.eof()) {
+        getline(ss, line);
+        stringstream s;
+        s << line;
+        string t;
+        s >> t;
+        s.ignore();
+        s.clear();
+        string ticker;
+        int shares;
+        double dividendRatio;
+        while (s << line) {
+            s >> ticker;
+            s << line;
+            s >> shares;
+            s << line;
+            s >> dividendRatio;
+            v.push_back(MyStock(ticker, shares, dividendRatio));
+        }
+        v.pop_back();
+    }
+    return v;
 }
 
-double getCurrentCash() {
-
+void showMySecurities() {
+    vector<MyStock> v = mySecurities();
+    for (int i=0; i<v.size(); i++) {
+        cout << "!" << v[i].ticker << " " << v[i].shares << " " << v[i].dividendRatio << endl;
+    }
 }
 
-void bid(string ticker, double price, int shareNum) {
-
+bool bid(string ticker, double price, int shares) {
+    ss << "BID " << ticker << " " << price << " " << shares << " " << endl;
+    string line;
+    if (ss.good() && !ss.eof()) {
+        getline(ss, line);
+        stringstream s;
+        string t;
+        s << line;
+        s >> t;
+        if (t == "BID_OUT") {
+            cout << "bidding done" << endl;
+            return true;
+        }
+        else {
+            cout << "bidding failed" << endl;
+            return false;
+        }
+    }
+    cout << "bidding failed" << endl;
+    return false;
 }
 
-void ask(string ticker, double price, int shareNum) {
-
+bool clearBid(string ticker) {
+    ss << "CLEAR_BID " << ticker << " " << endl;
+    string line;
+    if (ss.good() && !ss.eof()) {
+        getline(ss, line);
+        stringstream s;
+        string t;
+        s << line;
+        s >> t;
+        if (t == "CLEAR_BID_OUT") {
+            cout << "clearing bid done" << endl;
+            return true;
+        }
+        else {
+            cout << "clearing failed" << endl;
+            return false;
+        }
+    }
+    cout << "clearing failed" << endl;
+    return false;
 }
 
+bool ask(string ticker, double price, int shares) {
+    ss << "ASK " << ticker << " " << price << " " << shares << endl;
+    if (ss.good() && !ss.eof()) {
+        string line;
+        getline(ss, line);
+        stringstream s;
+        string t;
+        s << line;
+        s >> t;
+        if (t == "ASK_OUT") {
+            cout << "asking done" << endl;
+            return true;
+        }
+        else {
+            cout << "asking failed" << endl;
+            return false;
+        }
+    }
+    cout << "asking failed" << endl;
+    return false;
+}
+
+bool clearAsk(string ticker) {
+    ss << "CLEAR_ASK " << ticker << " " << endl;
+    string line;
+    if (ss.good() && !ss.eof()) {
+        getline(ss, line);
+        stringstream s;
+        string t;
+        s << line;
+        s >> t;
+        if (t == "CLEAR_ASK_OUT") {
+            cout << "clearing ask done" << endl;
+            return true;
+        }
+        else {
+            cout << "clearing failed" << endl;
+            return false;
+        }
+    }
+    cout << "clearing failed" << endl;
+    return false;
+}
+
+vector<MyOrder> orders(string who, string ticker) {
+    if (who == "MY") ss << "MY_ORDERS " << endl;
+    else ss << "ORDERS " << ticker << " " << endl;
+    string line;
+    vector<MyOrder> v;
+    if (ss.good() && !ss.eof()) {
+        getline(ss, line);
+        stringstream s;
+        s << line;
+        string t;
+        s >> t;
+        s.ignore();
+        s.clear();
+        string type;
+        string ticker;
+        double price;
+        int shares;
+        while (s << line) {
+            s >> type;
+            s << line;
+            s >> ticker;
+            s << line;
+            s >> price;
+            s << line;
+            s >> shares;
+            v.push_back(MyOrder(type, ticker, price, shares));
+        }
+        v.pop_back();
+    }
+    return v;
+}
+
+void showOrders(string who, string ticker) {
+    vector<MyOrder> v = orders(who, ticker);
+    for (int i=0; i<v.size(); i++) {
+        cout << "?" << v[i].type << " " << v[i].ticker << " " << v[i].price << " " << v[i].shares << endl;
+    }
+}
+
+vector<Stock> securities() {
+    ss << "SECURITIES " << endl;
+    string line;
+    vector<Stock> v;
+    if (ss.good() && !ss.eof()) {
+        getline(ss, line);
+        stringstream s;
+        s << line;
+        string t;
+        s >> t;
+        s.ignore();
+        s.clear();
+        string ticker;
+        double netWorth;
+        double dividendRatio;
+        double volatility;
+        while (s << line) {
+            s >> ticker;
+            s << line;
+            s >> netWorth;
+            s << line;
+            s >> dividendRatio;
+            s << line;
+            s >> volatility;
+            v.push_back(Stock(ticker, netWorth, dividendRatio, volatility));
+        }
+        v.pop_back();
+    }
+    return v;
+}
+
+void showSecurities() {
+    vector<Stock> v = securities();
+    for (int i=0; i<v.size(); i++) {
+        cout << "-" << v[i].ticker << " " << v[i].netWorth << " " << v[i].dividendRatio << " " << v[i].volatility << endl;
+    }
+}
+
+//vector<Order> orders(string ticker) {
+//    ss << "ORDERS " << ticker << " " << endl;
+//    string line;
+//    vector<Order> v;
+//    if (ss.good() && !ss.eof()) {
+//        getline(ss, line);
+//        stringstream s;
+//        s << line;
+//        string t;
+//        s >> t;
+//        s.ignore();
+//        s.clear();
+//        string type;
+//        double price;
+//        int shares;
+//        while (s << line) {
+//            s >> type;
+//            s << line;
+//            s >> price;
+//            s << line;
+//            s >> shares;
+//            v.push_back(Order(type, price, shares));
+//        }
+//        v.pop_back();
+//    }
+//    return v;
+//}
+//
+//void showOrders(string ticker) {
+//    vector<Order> v = orders(ticker);
+//    for (int i=0; i<v.size(); i++) {
+//        cout << "+" << v[i].type << " " << v[i].price << " " << v[i].shares << endl;
+//    }
+//}
 
 /*
 *
 */
 int main(int argc, char** argv) {
-	if (argc < 5) {
-		cout << "args: <user> <password> <host> <port> <command>" << endl;
-		cout << argc;
-		exit(1);
-	}
-
-
-	string host = "codebb.cloudapp.net";
-	int port = 17429;
-
-
-	ss.open(host, port);
-
-	// string command("");
-	// for (int i = 5; i < argc; i++) {
-	// 	command += argv[i];
-	// 	command += " ";
-	// }
-	// command;
-
-	//ss << name << " " << password << "\n" << endl;
-
-	InitialStocks(); //Call security
-
-	while(true) {
-		fetchStocks();
-		stock s = getHighestDividen();
-
-	}
-
-	//ss << name << " " << password << "\n" << command << "\nCLOSE_CONNECTION" << endl;
-
-	while (ss.good() && !ss.eof()) {
-		string line;
-		getline(ss, line);
-		cout << line << endl;
-	}
+	ss.open("codebb.cloudapp.net", 17429);
+    ss << "TripleG sjj" << "\n";
+    //--------------------------------------
+    
+    showMyCash();
+    showSecurities();
+    bid("BUD",12,50);
+    ask("BUD",20,50);
+    showOrders("MY", "");
+    showMySecurities();
+    clearBid("BUD");
+    showOrders("ALL", "BUD");
+    //--------------------------------------
+    ss << "\nCLOSE_CONNECTION" << endl;
 	return 0;
 }
